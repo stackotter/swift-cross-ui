12e6fb60f119ba739660ffa135d44f115920083d
NinjaCheetah <58050615+NinjaCheetah@users.noreply.github.com>
2022-03-20 09:44:09 -0400

    Added random number generator example

---
+++ Sources/RandomNumberGenerator/RandomNumberGenerator.swift
@@ -0,0 +1,39 @@
+import SwiftGtkUI
+
+class RNGState: AppState {
+    @Observed var minNum = 0
+    @Observed var maxNum = 100
+    @Observed var randomNumber = 0
+}
+
+@main
+struct RandomNumberGenerator: App {
+    let identifier = "com.stackotter.RandomNumberGenerator"
+    let state = RNGState()
+    
+    var body: some ViewContent {
+        VStack {
+            Text("") // Placeholder until padding is available
+            Text("Random Number: \(state.randomNumber)")
+            Button("Generate") {
+                state.randomNumber = Int.random(in: state.minNum...state.maxNum)
+            }
+            HStack {
+                Button("-5") { state.minNum -= 5 }
+                Button("-1") { state.minNum -= 1 }
+                Text("Minimum: \(state.minNum)")
+                Button("+1") { state.minNum += 1 }
+                Button("+5") { state.minNum += 5 }
+            }
+            HStack {
+                Button("-5") { state.maxNum -= 5 }
+                Button("-1") { state.maxNum -= 1 }
+                Text("Maximum: \(state.maxNum)")
+                Button("+1") { state.maxNum += 1 }
+                Button("+5") { state.maxNum += 5 }
+            }
+            Text("") // Placeholder until padding is available
+        }
+    }
+}
+
