adb9cb176955083f54885f721c8b962b9c331a49
NinjaCheetah <58050615+NinjaCheetah@users.noreply.github.com>
2022-03-22 21:29:15 -0400

    Reworked some of my comments

--- Sources/SwiftCrossUI/App.swift
+++ Sources/SwiftCrossUI/App.swift
@@ -25,8 +25,8 @@ public class WindowProperties {
     public var defaultHeight: Int
     public var resizable: Bool
     
-    /// This custom initalizer is used because the default initializer has the 'internal' protect level and can't be accessed from other files.
-    /// The window's title is the only required value. All other values will use their defaults if not set.
+    /// This custom initalizer is used because the default initializer has the 'internal' protection level and can't be accessed from other files.
+    /// The window's title is the only required value. All other values will use the previously hard-coded values if not set.
     public init(title: String, defaultWidth: Int? = nil, defaultHeight: Int? = nil, resizable: Bool? = nil)
     {
         self.title = title
