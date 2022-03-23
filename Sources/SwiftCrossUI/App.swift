a151b0b459d37a786fec065211d58c5f017cc84c
NinjaCheetah <58050615+NinjaCheetah@users.noreply.github.com>
2022-03-23 10:17:22 -0400

    Moved WindowProperties into WindowProperties.swift

--- Sources/SwiftCrossUI/App.swift
+++ Sources/SwiftCrossUI/App.swift
@@ -19,22 +19,6 @@ public protocol App {
     init()
 }
 
-public class WindowProperties {
-    public var title: String
-    public var defaultWidth: Int
-    public var defaultHeight: Int
-    public var resizable: Bool
-    
-    /// This custom initalizer is used because the default initializer has the 'internal' protection level and can't be accessed from other files.
-    /// The window's title is the only required value. All other values will use the previously hard-coded values if not set.
-    public init(title: String, defaultWidth: Int? = nil, defaultHeight: Int? = nil, resizable: Bool? = nil) {
-        self.title = title
-        self.defaultWidth = defaultWidth ?? 200
-        self.defaultHeight = defaultHeight ?? 150
-        self.resizable = resizable ?? true
-    }
-}
-
 public extension App {
     /// Runs the application.
     static func main() {
