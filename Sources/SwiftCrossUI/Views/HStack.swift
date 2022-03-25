2e575e05aef4f3d5aca5db2fed33ffe9785d957d
stackotter <stackotter@stackotter.dev>
2022-03-25 16:06:57 +1000

    Fix all linter violations

--- Sources/SwiftCrossUI/Views/HStack.swift
+++ Sources/SwiftCrossUI/Views/HStack.swift
@@ -1,12 +1,12 @@
 /// A horizontally oriented container. Similar to a `HStack` in SwiftUI.
 public struct HStack<Content: ViewContent>: View {
     public var body: Content
-    
+
     /// Creates a new HStack.
     public init(@ViewContentBuilder _ content: () -> Content) {
         body = content()
     }
-    
+
     public func asWidget(_ children: Content.Children) -> GtkWidget {
         let hStack = GtkButtonBox(orientation: .horizontal)
         for widget in children.widgets {
