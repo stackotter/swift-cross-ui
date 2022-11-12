6e9e5a50c24965034b125bf3e018a00c9b94412d
stackotter <stackotter@stackotter.dev>
2022-11-12 23:54:04 +1000

    Default to smallest possible window size that fits content

--- Sources/SwiftCrossUI/_App.swift
+++ Sources/SwiftCrossUI/_App.swift
@@ -10,13 +10,13 @@ class _App<AppRoot: App> {
         let gtkApp = GtkApplication(applicationId: app.identifier)
 
         gtkApp.run { window in
-            // The title of the window.
             window.title = self.app.windowProperties.title
-            // The default width and height of the window.
-            window.defaultSize = GtkSize(
-                width: self.app.windowProperties.defaultWidth,
-                height: self.app.windowProperties.defaultHeight)
-            // Whether or not the window's size can be changed.
+            if let size = self.app.windowProperties.defaultSize {
+                window.defaultSize = GtkSize(
+                    width: size.width,
+                    height: size.height
+                )
+            }
             window.resizable = self.app.windowProperties.resizable
 
             self.viewGraph = ViewGraph(for: self.app)
