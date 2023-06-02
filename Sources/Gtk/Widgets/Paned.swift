cc6be7829421188159b5aed397920825bae90be5
Casper Zandbergen <info@casperzandbergen.nl>
2023-06-02 17:30:14 +0200

    Added NavigationSplitView

---
+++ Sources/Gtk/Widgets/Paned.swift
@@ -0,0 +1,49 @@
+import CGtk
+
+open class Paned: Widget, Orientable {
+    public init(orientation: Orientation) {
+        super.init()
+        widgetPointer = gtk_paned_new(orientation.toGtkOrientation())
+    }
+
+    public var startChild: Widget? {
+        didSet {
+            gtk_paned_set_start_child(opaquePointer, startChild?.castedPointer())
+            startChild?.parentWidget = self
+        }
+    }
+
+    public var endChild: Widget? {
+        didSet {
+            gtk_paned_set_end_child(opaquePointer, endChild?.castedPointer())
+            endChild?.parentWidget = self
+        }
+    }
+
+    public var position: Int {
+        get {
+            Int(gtk_paned_get_position(opaquePointer))
+        }
+        set {
+            gtk_paned_set_position(opaquePointer, gint(newValue))
+        }
+    }
+
+    public var shrinkStartChild: Bool {
+        get {
+            gtk_paned_get_shrink_start_child(opaquePointer).toBool()
+        }
+        set {
+            gtk_paned_set_shrink_start_child(opaquePointer, newValue.toGBoolean())
+        }
+    }
+
+    public var shrinkEndChild: Bool {
+        get {
+            gtk_paned_get_shrink_end_child(opaquePointer).toBool()
+        }
+        set {
+            gtk_paned_set_shrink_end_child(opaquePointer, newValue.toGBoolean())
+        }
+    }
+}
---
+++ Sources/SwiftCrossUI/Views/NavigationSplitView.swift
@@ -0,0 +1,44 @@
+public struct NavigationSplitView<SideBar: View, MiddleBar: View, Detail: View>: View {
+    public var body: ViewContent3<SideBar, MiddleBar, Detail>
+}
+
+extension NavigationSplitView where MiddleBar == EmptyViewContent {
+    public init(@ViewContentBuilder sidebar: () -> SideBar,
+                @ViewContentBuilder detail: () -> Detail) {
+        body = ViewContent3(sidebar(), EmptyViewContent(), detail())
+    }
+}
+
+extension NavigationSplitView {
+    public init(@ViewContentBuilder sidebar: () -> SideBar,
+                @ViewContentBuilder content: () -> MiddleBar,
+                @ViewContentBuilder detail: () -> Detail) {
+        body = ViewContent3(sidebar(), content(), detail())
+    }
+
+    public func asWidget(_ children: ViewGraphNodeChildren3<SideBar, MiddleBar, Detail>) -> GtkPaned {
+        let widget = GtkPaned(orientation: .horizontal)
+        widget.startChild = children.child0.widget
+
+        if MiddleBar.self == EmptyViewContent.self {
+            widget.endChild = children.child2.widget
+        } else {
+            let next = GtkPaned(orientation: .horizontal)
+            next.startChild = children.child1.widget
+            next.endChild = children.child2.widget
+            next.shrinkStartChild = false
+            next.shrinkEndChild = false
+            next.position = 0
+            next.expandVertically = true
+
+            widget.endChild = next
+        }
+
+        widget.shrinkStartChild = false
+        widget.shrinkEndChild = false
+        widget.position = 0 // sets
+        widget.expandVertically = true
+
+        return widget
+    }
+}
