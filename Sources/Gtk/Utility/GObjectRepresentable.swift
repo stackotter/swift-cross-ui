56f58b4f2b09fd704ef1d033031e2817ebb76071
Casper Zandbergen <info@casperzandbergen.nl>
2023-06-06 16:36:58 +0200

    move comment to doc

--- Sources/Gtk/Utility/GObjectRepresentable.swift
+++ Sources/Gtk/Utility/GObjectRepresentable.swift
@@ -12,16 +12,17 @@ extension GObjectRepresentable {
         g_object_set_property(gobjectPointer, named, value)
     }
 
+    /// If this returned an optional at runtime and crashed; it is because the
+    /// underlying value was nil while `V` was not `Optional<GValueRepresentable>`.
+    /// Change to optional at caller site. eg:
+    /// ```diff
+    /// -@GObjectProperty(named: "optional-value") var value: String
+    /// +@GObjectProperty(named: "optional-value") var value: String?
+    /// ```
     public func getProperty<V: GValueRepresentable>(named: String) -> V {
         var v = GValue()
         let value = g_value_init(&v, V.type)!
         g_object_get_property(gobjectPointer, named, value)
-        // If this returned an optional at runtime and crashed it is because the
-        // underlying value was nil while `V` was not `Optional<GValueRepresentable>`.
-        // Change to optional at caller site. eg:
-        //
-        // -@GObjectProperty(named: "optional-value") var value: String
-        // +@GObjectProperty(named: "optional-value") var value: String?
         return V(value)!
     }
 }
