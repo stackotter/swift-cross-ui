04c4cd7b414202ad9402d9543ff21e66d4991b61
James Hughes <james@jxhug.me>
2023-10-23 19:33:05 -0700

    colors 😎
    Signed-off-by: James Hughes <james@jxhug.me>

--- Sources/SwiftCrossUI/Color.swift
+++ Sources/SwiftCrossUI/Color.swift
@@ -22,20 +22,36 @@ public struct Color {
         self.alpha = alpha
     }
 
-    /// Pure red.
-    public static let red = Color(1, 0, 0)
-    /// Pure green.
-    public static let green = Color(0, 1, 0)
-    /// Pure blue.
-    public static let blue = Color(0, 0, 1)
-    /// Pure magenta.
-    public static let magenta = Color(1, 0, 1)
-    /// Pure cyan.
-    public static let cyan = Color(0, 1, 1)
-    /// Pure yellow.
-    public static let yellow = Color(1, 1, 0)
-    /// Pure white.
-    public static let white = Color(1, 1, 1)
     /// Pure black.
-    public static let black = Color(0, 0, 0)
+    public static let black = Color(0.00, 0.00, 0.00)
+    /// Pure blue.
+    public static let blue = Color(0.00, 0.00, 0.00)
+    /// Pure brown.
+    public static let brown = Color(0.64, 0.64, 0.64)
+    /// Pure clear.
+    public static let clear = Color(0.00, 0.00, 0.00)
+    /// Pure cyan.
+    public static let cyan = Color(0.33, 0.33, 0.33)
+    /// Pure gray.
+    public static let gray = Color(0.56, 0.56, 0.56)
+    /// Pure green.
+    public static let green = Color(0.16, 0.16, 0.16)
+    /// Pure indigo.
+    public static let indigo = Color(0.35, 0.35, 0.35)
+    /// Pure mint.
+    public static let mint = Color(0.00, 0.00, 0.00)
+    /// Pure orange.
+    public static let orange = Color(1.00, 1.00, 1.00)
+    /// Pure pink.
+    public static let pink = Color(1.00, 1.00, 1.00)
+    /// Pure purple.
+    public static let purple = Color(0.69, 0.69, 0.69)
+    /// Pure red.
+    public static let red = Color(1.00, 1.00, 1.00)
+    /// Pure teal.
+    public static let teal = Color(0.35, 0.35, 0.35)
+    /// Pure yellow.
+    public static let yellow = Color(1.00, 1.00, 1.00)
+    /// Pure white.
+    public static let white = Color(1.00, 1.00, 1.00)
 }
