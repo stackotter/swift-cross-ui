418250f407dfb3620cf8b460dba4841756908104
James Hughes <james@jxhug.me>
2023-10-23 19:33:05 -0700

    Add all SwiftUI colors
    Signed-off-by: James Hughes <james@jxhug.me>

--- Sources/SwiftCrossUI/Color.swift
+++ Sources/SwiftCrossUI/Color.swift
@@ -21,21 +21,37 @@ public struct Color {
         self.blue = blue
         self.alpha = alpha
     }
-
-    /// Pure red.
-    public static let red = Color(1, 0, 0)
-    /// Pure green.
-    public static let green = Color(0, 1, 0)
-    /// Pure blue.
-    public static let blue = Color(0, 0, 1)
-    /// Pure magenta.
-    public static let magenta = Color(1, 0, 1)
-    /// Pure cyan.
-    public static let cyan = Color(0, 1, 1)
-    /// Pure yellow.
-    public static let yellow = Color(1, 1, 0)
-    /// Pure white.
-    public static let white = Color(1, 1, 1)
+    
     /// Pure black.
-    public static let black = Color(0, 0, 0)
+    public static let black = Color(0.00, 0.00, 0.00)
+    /// Pure blue.
+    public static let blue = Color(0.00, 0.48, 1.00)
+    /// Pure brown.
+    public static let brown = Color(0.64, 0.52, 0.37)
+    /// Pure clear.
+    public static let clear = Color(0.00, 0.00, 0.00)
+    /// Pure cyan.
+    public static let cyan = Color(0.33, 0.75, 0.94)
+    /// Pure gray.
+    public static let gray = Color(0.56, 0.56, 0.58)
+    /// Pure green.
+    public static let green = Color(0.16, 0.80, 0.25)
+    /// Pure indigo.
+    public static let indigo = Color(0.35, 0.34, 0.84)
+    /// Pure mint.
+    public static let mint = Color(0.00, 0.78, 0.75)
+    /// Pure orange.
+    public static let orange = Color(1.00, 0.58, 0.00)
+    /// Pure pink.
+    public static let pink = Color(1.00, 0.18, 0.33)
+    /// Pure purple.
+    public static let purple = Color(0.69, 0.32, 0.87)
+    /// Pure red.
+    public static let red = Color(1.00, 0.23, 0.19)
+    /// Pure teal.
+    public static let teal = Color(0.35, 0.68, 0.77)
+    /// Pure yellow.
+    public static let yellow = Color(1.00, 0.80, 0.00)
+    /// Pure white.
+    public static let white = Color(1.00, 1.00, 1.00)
 }
