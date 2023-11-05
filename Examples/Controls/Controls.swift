b64a34cba5e084b3aeeeefe106c5d47cffade892
NinjaCheetah <58050615+NinjaCheetah@users.noreply.github.com>
2023-11-05 14:10:47 -0500

    Reworked ControlsExample to be a lot more visually clean

--- Examples/Controls/Controls.swift
+++ Examples/Controls/Controls.swift
@@ -1,7 +1,13 @@
 import GtkBackend
 import SwiftCrossUI
 
+enum ControlCategory {
+    case buttons
+    case toggles
+}
+
 class ControlsState: Observable {
+    @Observed var selectedCategory: ControlCategory?
     @Observed var count = 0
     @Observed var exampleButtonState = false
     @Observed var exampleSwitchState = false
@@ -17,37 +23,60 @@ struct ControlsApp: App {
 
     let windowProperties = WindowProperties(
         title: "ControlsApp",
+        defaultSize: WindowProperties.Size(400, 350),
         resizable: true
     )
 
     var body: some View {
-        HStack {
-            VStack {
-                Text("Button")
-                Button("Click me!") {
-                    state.count += 1
+        NavigationSplitView {
+            VStack() {
+                Button("Buttons") { state.selectedCategory = .buttons }
+                Button("Toggles") { state.selectedCategory = .toggles }
+                Spacer()
+            }.padding(10)
+        } detail: {
+            VStack() {
+                switch state.selectedCategory {
+                    case .buttons:
+                        ButtonsView
+                            .padding(.bottom, 10)
+                    case .toggles:
+                        TogglesView
+                            .padding(.bottom, 10)
+                    case nil:
+                        Text("Select a category.")
                 }
-                Text("Count: \(state.count)", wrap: false)
-                Spacer()
-            }
-            Spacer()
-            VStack {
-                Text("Toggle (Button Style)")
-                Toggle("Toggle me!", active: state.$exampleButtonState)
-                    .toggleStyle(.button)
-                Text("Currently enabled: \(state.exampleButtonState)")
-                Spacer()
-                    .padding(.bottom, 10)
-                Text("Toggle (Switch Style)")
-                HStack {
-                    Toggle("Toggle me:", active: state.$exampleSwitchState)
-                        .toggleStyle(.switch)
-                    Spacer()
-                }
-                Text("Currently enabled: \(state.exampleSwitchState)")
-                Spacer()
-            }
+            }.padding(10)
         }
-        .padding(10)
+    }
+
+    var ButtonsView: some View {
+        VStack {
+            Text("Button")
+            Button("Click me!") {
+                state.count += 1
+            }
+            Text("Count: \(state.count)", wrap: false)
+            Spacer()
+        }
+    }
+
+    var TogglesView: some View {
+        VStack {
+            Text("Toggle (Button Style)")
+            Toggle("Toggle me!", active: state.$exampleButtonState)
+                .toggleStyle(.button)
+            Text("Currently enabled: \(state.exampleButtonState)")
+            Spacer()
+                .padding(.bottom, 10)
+            Text("Toggle (Switch Style)")
+            HStack {
+                Toggle("Toggle me:", active: state.$exampleSwitchState)
+                    .toggleStyle(.switch)
+                Spacer()
+            }
+            Text("Currently enabled: \(state.exampleSwitchState)")
+            Spacer()
+        }
     }
 }
