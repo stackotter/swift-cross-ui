7c3373ce74f3cecf23e52834139e816b4b6ed6e6
NinjaCheetah <58050615+NinjaCheetah@users.noreply.github.com>
2023-11-06 09:06:32 -0500

    Resolved most requested changes selectedToggleStyle now a var, formatting issues have been fixed Private structs no longer private, init property removed, modifier changed to use variable

--- Examples/Controls/Controls.swift
+++ Examples/Controls/Controls.swift
@@ -1,13 +1,7 @@
 import GtkBackend
 import SwiftCrossUI
 
-enum ControlCategory {
-    case buttons
-    case toggles
-}
-
 class ControlsState: Observable {
-    @Observed var selectedCategory: ControlCategory?
     @Observed var count = 0
     @Observed var exampleButtonState = false
     @Observed var exampleSwitchState = false
@@ -23,60 +17,38 @@ struct ControlsApp: App {
 
     let windowProperties = WindowProperties(
         title: "ControlsApp",
-        defaultSize: WindowProperties.Size(400, 350),
         resizable: true
     )
 
     var body: some View {
-        NavigationSplitView {
-            VStack() {
-                Button("Buttons") { state.selectedCategory = .buttons }
-                Button("Toggles") { state.selectedCategory = .toggles }
-                Spacer()
-            }.padding(10)
-        } detail: {
-            VStack() {
-                switch state.selectedCategory {
-                    case .buttons:
-                        ButtonsView
-                            .padding(.bottom, 10)
-                    case .toggles:
-                        TogglesView
-                            .padding(.bottom, 10)
-                    case nil:
-                        Text("Select a category.")
+        HStack {
+            VStack {
+                Text("Button")
+                Button("Click me!") {
+                    state.count += 1
                 }
-            }.padding(10)
-        }
-    }
-
-    var ButtonsView: some View {
-        VStack {
-            Text("Button")
-            Button("Click me!") {
-                state.count += 1
-            }
-            Text("Count: \(state.count)", wrap: false)
-            Spacer()
-        }
-    }
-
-    var TogglesView: some View {
-        VStack {
-            Text("Toggle (Button Style)")
-            Toggle("Toggle me!", active: state.$exampleButtonState)
-                .toggleStyle(.button)
-            Text("Currently enabled: \(state.exampleButtonState)")
-            Spacer()
-                .padding(.bottom, 10)
-            Text("Toggle (Switch Style)")
-            HStack {
-                Toggle("Toggle me:", active: state.$exampleSwitchState)
-                    .toggleStyle(.switch)
+                Text("Count: \(state.count)", wrap: false)
                 Spacer()
             }
-            Text("Currently enabled: \(state.exampleSwitchState)")
             Spacer()
+            VStack {
+                Text("Toggle (Button Style)")
+                Toggle("Toggle me!", active: state.$exampleButtonState)
+                    .toggleStyle(.button)
+                Text("Currently enabled: \(state.exampleButtonState)")
+                Spacer()
+                    .padding(.bottom, 10)
+                Text("Toggle (Switch Style)")
+                HStack {
+                    Toggle("Toggle me:", active: state.$exampleSwitchState)
+                        .toggleStyle(.switch)
+                    Spacer()
+                        .padding(.bottom, 10)
+                }
+                Text("Currently enabled: \(state.exampleSwitchState)")
+                Spacer()
+            }
         }
+        .padding(10)
     }
 }
--- Sources/SwiftCrossUI/Views/Toggle.swift
+++ Sources/SwiftCrossUI/Views/Toggle.swift
@@ -1,15 +1,14 @@
-
 public struct Toggle: View {
     /// The style of toggle shown.
-    public let selectedToggleStyle: ToggleStyle
+    var selectedToggleStyle: ToggleStyle
     /// The label to be shown on or beside the toggle.
     var label: String
     /// Whether the toggle is active or not.
     var active: Binding<Bool>
 
     /// Creates a toggle that displays a custom label.
-    public init(selectedToggleStyle: ToggleStyle = .button, _ label: String, active: Binding<Bool>) {
-        self.selectedToggleStyle = selectedToggleStyle
+    public init(_ label: String, active: Binding<Bool>) {
+        self.selectedToggleStyle = .button
         self.label = label
         self.active = active
     }
@@ -23,91 +22,20 @@ public struct Toggle: View {
                 }
             case .button:
                 ToggleButton(label, active: active)
+        }
     }
-  }
 }
 
 extension Toggle {
     /// Sets the style of the toggle.
     public func toggleStyle(_ selectedToggleStyle: ToggleStyle) -> Toggle {
-        return Toggle(
-            selectedToggleStyle: selectedToggleStyle, 
-            self.label, 
-            active: self.active
-        )
+        var toggle = self
+        toggle.selectedToggleStyle = selectedToggleStyle
+        return toggle
     }
 }
 
 public enum ToggleStyle {
     case `switch`
     case button
-}
-
-/// A light switch style control that is either on or off.
-private struct ToggleSwitch: ElementaryView, View {
-    /// Whether the switch is active or not.
-    private var active: Binding<Bool>
-
-    /// Creates a switch.
-    public init(active: Binding<Bool>) {
-        self.active = active
-    }
-
-    public func asWidget<Backend: AppBackend>(
-        backend: Backend
-    ) -> Backend.Widget {
-        return backend.createSwitch(
-            active: active.wrappedValue,
-            onChange: { newValue in
-                self.active.wrappedValue = newValue
-            }
-        )
-    }
-
-    public func update<Backend: AppBackend>(
-        _ widget: Backend.Widget,
-        backend: Backend
-    ) {
-        backend.setIsActive(ofSwitch: widget, to: active.wrappedValue)
-        backend.setOnChange(ofSwitch: widget) { newActiveState in
-            active.wrappedValue = newActiveState
-        }
-    }
-}
-
-/// A button style control that is either on or off.
-private struct ToggleButton: ElementaryView, View {
-    /// The label to show on the toggle button.
-    private var label: String
-    /// Whether the button is active or not.
-    private var active: Binding<Bool>
-
-    /// Creates a toggle button that displays a custom label.
-    public init(_ label: String, active: Binding<Bool>) {
-        self.label = label
-        self.active = active
-    }
-
-    public func asWidget<Backend: AppBackend>(
-        backend: Backend
-    ) -> Backend.Widget {
-        return backend.createToggle(
-            label: label,
-            active: active.wrappedValue,
-            onChange: { newValue in
-                self.active.wrappedValue = newValue
-            }
-        )
-    }
-
-    public func update<Backend: AppBackend>(
-        _ widget: Backend.Widget,
-        backend: Backend
-    ) {
-        backend.setLabel(ofButton: widget, to: label)
-        backend.setIsActive(ofToggle: widget, to: active.wrappedValue)
-        backend.setOnChange(ofToggle: widget) { newActiveState in
-            active.wrappedValue = newActiveState
-        }
-    }
 }
