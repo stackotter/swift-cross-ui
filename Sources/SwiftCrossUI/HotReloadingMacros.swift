4f745620561c3ecfaeceaf324ed04d06a49ceb82
stackotter <stackotter@stackotter.dev>
2024-08-06 17:42:54 +1000

    Reimplement HotReloadableView under new layout system (kind of... still has many issues)

--- Sources/SwiftCrossUI/HotReloadingMacros.swift
+++ Sources/SwiftCrossUI/HotReloadingMacros.swift
@@ -1,24 +1,24 @@
-// import Foundation
+import Foundation
 
-// @attached(
-//     peer,
-//     names: named(hotReloadingExportedEntryPoint),
-//     named(hotReloadingImportedEntryPoint),
-//     named(hotReloadingHasConnectedToServer))
-// @attached(member, names: named(entryPoint), named(hotReloadingExprIds))
-// public macro HotReloadable() =
-//     #externalMacro(module: "HotReloadingMacrosPlugin", type: "HotReloadableAppMacro")
+@attached(
+    peer,
+    names: named(hotReloadingExportedEntryPoint),
+    named(hotReloadingImportedEntryPoint),
+    named(hotReloadingHasConnectedToServer))
+@attached(member, names: named(entryPoint), named(hotReloadingExprIds))
+public macro HotReloadable() =
+    #externalMacro(module: "HotReloadingMacrosPlugin", type: "HotReloadableAppMacro")
 
-// @freestanding(expression)
-// public macro hotReloadable<T: View>(@ViewBuilder _ expr: () -> T) -> HotReloadableView =
-//     #externalMacro(module: "HotReloadingMacrosPlugin", type: "HotReloadableExprMacro")
+@freestanding(expression)
+public macro hotReloadable<T: View>(@ViewBuilder _ expr: () -> T) -> HotReloadableView =
+    #externalMacro(module: "HotReloadingMacrosPlugin", type: "HotReloadableExprMacro")
 
-// public struct ExprLocation: Hashable {
-//     var line: Int
-//     var column: Int
+public struct ExprLocation: Hashable {
+    var line: Int
+    var column: Int
 
-//     public init(line: Int, column: Int) {
-//         self.line = line
-//         self.column = column
-//     }
-// }
+    public init(line: Int, column: Int) {
+        self.line = line
+        self.column = column
+    }
+}
--- Sources/SwiftCrossUI/Views/NavigationLink.swift
+++ Sources/SwiftCrossUI/Views/NavigationLink.swift
@@ -4,26 +4,26 @@
 ///
 /// Unlike Apples SwiftUI API a `NavigationLink` can be outside of a `NavigationStack`
 /// as long as they share the same `NavigationPath`.
-// public struct NavigationLink: View {
-//     public var body: some View {
-//         Button(label) {
-//             path.wrappedValue.append(value)
-//         }
-//     }
+public struct NavigationLink: View {
+    public var body: some View {
+        Button(label) {
+            path.wrappedValue.append(value)
+        }
+    }
 
-//     /// The label to display on the button.
-//     private let label: String
-//     /// The value to append to the navigation path when clicked.
-//     private let value: any Codable
-//     /// The navigation path to append to when clicked.
-//     private let path: Binding<NavigationPath>
+    /// The label to display on the button.
+    private let label: String
+    /// The value to append to the navigation path when clicked.
+    private let value: any Codable
+    /// The navigation path to append to when clicked.
+    private let path: Binding<NavigationPath>
 
-//     /// Creates a navigation link that presents the view corresponding to a value.
-//     /// The link is handled by whatever ``NavigationStack`` is sharing the same
-//     /// navigation path.
-//     public init<C: Codable>(_ label: String, value: C, path: Binding<NavigationPath>) {
-//         self.label = label
-//         self.value = value
-//         self.path = path
-//     }
-// }
+    /// Creates a navigation link that presents the view corresponding to a value.
+    /// The link is handled by whatever ``NavigationStack`` is sharing the same
+    /// navigation path.
+    public init<C: Codable>(_ label: String, value: C, path: Binding<NavigationPath>) {
+        self.label = label
+        self.value = value
+        self.path = path
+    }
+}
