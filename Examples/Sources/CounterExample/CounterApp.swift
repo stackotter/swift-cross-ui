d348badb71884b26980f20d6933f0b519c840c33
stackotter <stackotter@stackotter.dev>
2024-12-10 17:25:17 +1000

    Revert accidentally committed CounterApp changes (from local testing)

--- Examples/Sources/CounterExample/CounterApp.swift
+++ Examples/Sources/CounterExample/CounterApp.swift
@@ -30,30 +30,5 @@ struct CounterApp: App {
             }
         }
         .defaultSize(width: 400, height: 200)
-        .commands {
-            CommandMenu("Help") {
-                Text("Need help?")
-                Button("User guide") {
-                    print("User guide")
-                }
-                Menu("Export as...") {
-                    Button("PNG") {
-                        print("Export as PNG")
-                    }
-                    Button("JPEG") {
-                        print("Export as JPEG")
-                    }
-                }
-            }
-
-            CommandMenu("Window") {
-                Button("Maximize") {
-                    print("Maximize")
-                }
-                Button("Minimize") {
-                    print("Minimize")
-                }
-            }
-        }
     }
 }
