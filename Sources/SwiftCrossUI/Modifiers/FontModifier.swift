aaa935cf3c94114787af17f5b9b4ab375c194503
stackotter <stackotter@stackotter.dev>
2024-12-15 01:04:20 +1000

    Implement View.bold() modifier

--- Sources/SwiftCrossUI/Modifiers/FontModifier.swift
+++ Sources/SwiftCrossUI/Modifiers/FontModifier.swift
@@ -1,8 +1,22 @@
 extension View {
     public func font(_ font: Font) -> some View {
         EnvironmentModifier(self) { environment in
-            // TODO: Merge fonts?
             environment.with(\.font, font)
+        }
+    }
+
+    public func bold() -> some View {
+        EnvironmentModifier(self) { environment in
+            let font =
+                switch environment.font {
+                    case let .system(size, _, design):
+                        Font.system(size: size, weight: .bold, design: design)
+                }
+
+            return environment.with(
+                \.font,
+                font
+            )
         }
     }
 }
