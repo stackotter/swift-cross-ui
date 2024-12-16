352c2ce257993edf25e1c94edf9779a5492e2124
stackotter <stackotter@stackotter.dev>
2024-12-16 13:58:22 +1000

    Chain onSubmit handlers (running outermost ones first to match SwiftUI)

--- Sources/SwiftCrossUI/Modifiers/OnSubmitModifier.swift
+++ Sources/SwiftCrossUI/Modifiers/OnSubmitModifier.swift
@@ -1,9 +1,15 @@
 extension View {
     /// Adds an action to perform when the user submits a text field within this
-    /// view (generally via pressing the Enter/Return key).
+    /// view (generally via pressing the Enter/Return key). Outer `onSubmit`
+    /// handlers get called before inner `onSubmit` handlers. To prevent
+    /// submissions from propagating upwards, use ``View/submitScope()`` after
+    /// adding the handler.
     public func onSubmit(perform action: @escaping () -> Void) -> some View {
         EnvironmentModifier(self) { environment in
-            environment.with(\.onSubmit, action)
+            environment.with(\.onSubmit) {
+                environment.onSubmit?()
+                action()
+            }
         }
     }
 
