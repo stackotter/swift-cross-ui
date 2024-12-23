73a101b13a65309eba744a07f8e90556f3424147
stackotter <stackotter@stackotter.dev>
2024-12-23 16:28:25 +1000

    Parse new Swift Bundler additionalMetadata field when present in binary

--- Sources/SwiftCrossUI/App.swift
+++ Sources/SwiftCrossUI/App.swift
@@ -34,6 +34,24 @@ public var _forceRefresh: () -> Void = {}
 /// Metadata embedded by Swift Bundler if present. Loaded at app start up.
 private var swiftBundlerAppMetadata: AppMetadata?
 
+/// An error encountered when parsing Swift Bundler metadata.
+private enum SwiftBundlerMetadataError: LocalizedError {
+    case jsonNotDictionary(Any)
+    case missingAppIdentifier
+    case missingAppVersion
+
+    var errorDescription: String? {
+        switch self {
+            case .jsonNotDictionary:
+                "Root metadata JSON value wasn't an object"
+            case .missingAppIdentifier:
+                "Missing 'appIdentifier' (of type String)"
+            case .missingAppVersion:
+                "Missing 'appVersion' (of type String)"
+        }
+    }
+}
+
 extension App {
     /// Metadata loaded at app start up.
     public static var metadata: AppMetadata? {
@@ -78,9 +96,26 @@ extension App {
         let jsonData = Data(bytes[jsonStart..<lengthStart])
 
         do {
-            return try JSONDecoder().decode(
-                AppMetadata.self,
-                from: jsonData
+            // Manually parsed due to the `additionalMetadata` field (which would
+            // require a lot of boilerplate code to parse with Codable).
+            let jsonValue = try JSONSerialization.jsonObject(with: jsonData)
+            guard let json = jsonValue as? [String: Any] else {
+                throw SwiftBundlerMetadataError.jsonNotDictionary(jsonValue)
+            }
+            guard let identifier = json["appIdentifier"] as? String else {
+                throw SwiftBundlerMetadataError.missingAppIdentifier
+            }
+            guard let version = json["appVersion"] as? String else {
+                throw SwiftBundlerMetadataError.missingAppVersion
+            }
+            let additionalMetadata =
+                json["additionalMetadata"].map { value in
+                    value as? [String: Any] ?? [:]
+                } ?? [:]
+            return AppMetadata(
+                identifier: identifier,
+                version: version,
+                additionalMetadata: additionalMetadata
             )
         } catch {
             print("warning: Swift Bundler metadata present but couldn't be parsed")
--- Sources/SwiftCrossUI/AppMetadata.swift
+++ Sources/SwiftCrossUI/AppMetadata.swift
@@ -1,17 +1,19 @@
 /// Metadata loaded at app start up.
-public struct AppMetadata: Codable {
+public struct AppMetadata {
     /// The app's reverse domain name identifier.
     public var identifier: String
     /// The app's version (generally a semantic version string).
     public var version: String
+    /// Additional developer-defined metadata.
+    public var additionalMetadata: [String: Any]
 
-    public init(identifier: String, version: String) {
+    public init(
+        identifier: String,
+        version: String,
+        additionalMetadata: [String: Any]
+    ) {
         self.identifier = identifier
         self.version = version
-    }
-
-    private enum CodingKeys: String, CodingKey {
-        case identifier = "appIdentifier"
-        case version = "appVersion"
+        self.additionalMetadata = additionalMetadata
     }
 }
