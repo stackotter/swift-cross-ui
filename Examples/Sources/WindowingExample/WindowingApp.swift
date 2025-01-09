e2bbfdd5032c59bca1877d4874d58473f77ea120
stackotter <stackotter@stackotter.dev>
2025-01-09 23:54:39 +1000

    Add file dialogs, alerts, and menu items to WindowingExample

--- Examples/Sources/WindowingExample/WindowingApp.swift
+++ Examples/Sources/WindowingExample/WindowingApp.swift
@@ -6,11 +6,70 @@ import SwiftCrossUI
     import SwiftBundlerRuntime
 #endif
 
+struct FileDialogDemo: View {
+    @State var selectedFile: URL? = nil
+    @State var saveDestination: URL? = nil
+
+    @Environment(\.chooseFile) var chooseFile
+    @Environment(\.chooseFileSaveDestination) var chooseFileSaveDestination
+
+    var body: some View {
+        Text("File dialog demo")
+            .font(.system(size: 18))
+
+        if let selectedFile {
+            Text("Selected file: \(selectedFile.path)")
+        } else {
+            Text("No file selected")
+        }
+
+        if let saveDestination {
+            Text("Save destination: \(saveDestination.path)")
+        }
+
+        HStack {
+            Button("Open") {
+                Task {
+                    guard let file = await chooseFile() else {
+                        return
+                    }
+                    selectedFile = file
+                }
+            }
+
+            Button("Save") {
+                Task {
+                    guard let file = await chooseFileSaveDestination() else {
+                        return
+                    }
+                    saveDestination = file
+                }
+            }
+        }
+    }
+}
+
+struct AlertDemo: View {
+    @Environment(\.presentAlert) var presentAlert
+
+    var body: some View {
+        Text("Alert demo")
+            .font(.system(size: 18))
+
+        Button("Present error") {
+            Task {
+                await presentAlert("Failed to succeed")
+            }
+        }
+    }
+}
+
 @main
 @HotReloadable
 struct WindowingApp: App {
     @State var title = "My window"
     @State var resizable = false
+    @State var errorMessage: String? = nil
 
     var body: some Scene {
         WindowGroup(title) {
@@ -20,16 +79,36 @@ struct WindowingApp: App {
                         Text("Window title:")
                         TextField("My window", $title)
                     }
+
                     Button(resizable ? "Disable resizing" : "Enable resizing") {
                         resizable = !resizable
                     }
+
                     Image(Bundle.module.bundleURL.appendingPathComponent("Banner.png"))
+
+                    Divider()
+
+                    FileDialogDemo()
+
+                    Divider()
+
+                    AlertDemo()
                 }
-                .padding(10)
+                .padding(20)
             }
         }
         .defaultSize(width: 500, height: 500)
         .windowResizability(resizable ? .contentMinSize : .contentSize)
+        .commands {
+            CommandMenu("Demo menu") {
+                Button("Menu item") {}
+
+                Menu("Submenu") {
+                    Button("Item 1") {}
+                    Button("Item 2") {}
+                }
+            }
+        }
 
         WindowGroup("Secondary window") {
             #hotReloadable {
