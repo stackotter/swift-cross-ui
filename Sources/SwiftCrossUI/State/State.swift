f5cc3fe421cd08178d6c8a10bf1f222fabda959b
stackotter <stackotter@stackotter.dev>
2025-01-15 23:38:22 +1000

    Fix value type State restoration, sync the box not the value (#101?)
    This led to bindings pointing to stale state in some cases. Hopefully
    fixes #101 but I haven't reproduced the issue locally so not guaranteed.

--- Sources/SwiftCrossUI/State/State.swift
+++ Sources/SwiftCrossUI/State/State.swift
@@ -3,11 +3,17 @@ import Foundation
 @propertyWrapper
 public struct State<Value>: DynamicProperty, StateProperty {
     class Storage {
-        var value: Value
+        // This inner box is what stays constant between view updates. The
+        // outer box (Storage) is used so that we can assign this box to
+        // future state instances from the non-mutating
+        // `update(with:previousValue:)` method. It's vital that the inner
+        // box remains the same so that bindings can be stored across view
+        // updates.
+        var box: Box<Value>
         var didChange = Publisher()
 
         init(_ value: Value) {
-            self.value = value
+            self.box = Box(value: value)
         }
     }
 
@@ -19,21 +25,24 @@ public struct State<Value>: DynamicProperty, StateProperty {
 
     public var wrappedValue: Value {
         get {
-            storage.value
+            storage.box.value
         }
         nonmutating set {
-            storage.value = newValue
+            storage.box.value = newValue
             didChange.send()
         }
     }
 
     public var projectedValue: Binding<Value> {
-        Binding(
+        // Specifically link the binding to the inner box instead of the outer
+        // storage which changes with each view update.
+        let box = storage.box
+        return Binding(
             get: {
-                storage.value
+                box.value
             },
             set: { newValue in
-                storage.value = newValue
+                box.value = newValue
                 didChange.send()
             }
         )
@@ -49,8 +58,8 @@ public struct State<Value>: DynamicProperty, StateProperty {
 
     public func update(with environment: EnvironmentValues, previousValue: State<Value>?) {
         if let previousValue {
-            storage.value = previousValue.storage.value
-            storage.didChange = previousValue.didChange
+            storage.box = previousValue.storage.box
+            storage.didChange = previousValue.storage.didChange
         }
     }
 
@@ -62,11 +71,11 @@ public struct State<Value>: DynamicProperty, StateProperty {
             return
         }
 
-        storage.value = state as! Value
+        storage.box.value = state as! Value
     }
 
     func snapshot() throws -> Data? {
-        if let value = storage.value as? Codable {
+        if let value = storage.box as? Codable {
             return try JSONEncoder().encode(value)
         } else {
             return nil
