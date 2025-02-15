be74a9ebb6757ce18db348dd32ec56940ea1aa81
furby™ <devs@wabi.foundation>
2025-02-15 02:17:22 -0700

    AppKit: specify main actors on NSViewRepresentable.
    Signed-off-by: furby™ <devs@wabi.foundation>

--- Sources/AppKitBackend/NSViewRepresentable.swift
+++ Sources/AppKitBackend/NSViewRepresentable.swift
@@ -12,6 +12,7 @@ where Content == Never {
     associatedtype Coordinator = Void
 
     /// Create the initial NSView instance.
+    @MainActor
     func makeNSView(context: NSViewRepresentableContext<Coordinator>) -> NSViewType
 
     /// Update the view with new values.
@@ -20,12 +21,14 @@ where Content == Never {
     ///   - context: The context, including the coordinator and potentially new environment
     ///   values.
     /// - Note: This may be called even when `context` has not changed.
+    @MainActor
     func updateNSView(_ nsView: NSViewType, context: NSViewRepresentableContext<Coordinator>)
 
     /// Make the coordinator for this view.
     ///
     /// The coordinator is used when the view needs to communicate changes to the rest of
     /// the view hierarchy (i.e. through bindings), and is often the view's delegate.
+    @MainActor
     func makeCoordinator() -> Coordinator
 
     /// Compute the view's size.
