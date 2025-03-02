6fd98a7bc00b740f33eece9af7942e838d167d28
furby™ <devs@wabi.foundation>
2025-03-01 17:41:47 -0700

    Add `NSViewRepresentable` for AppKit & support Swift 6 concurrency. (#105)
    Co-authored-by: stackotter <stackotter@stackotter.dev>

--- Package.swift
+++ Package.swift
@@ -95,7 +95,7 @@ let package = Package(
         ),
         .package(
             url: "https://github.com/stackotter/swift-image-formats",
-            .upToNextMinor(from: "0.3.1")
+            .upToNextMinor(from: "0.3.2")
         ),
         .package(
             url: "https://github.com/wabiverse/swift-windowsappsdk",
