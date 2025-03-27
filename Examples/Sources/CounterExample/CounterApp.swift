5240b06a0653950917860b92812dca058d036661
stackotter <stackotter@stackotter.dev>
2025-03-27 14:07:44 +1000

    Document UIKitBackend menu item ordering

--- Examples/Sources/CounterExample/CounterApp.swift
+++ Examples/Sources/CounterExample/CounterApp.swift
@@ -1,4 +1,5 @@
 import DefaultBackend
+import Foundation
 import SwiftCrossUI
 
 #if canImport(SwiftBundlerRuntime)
@@ -9,20 +10,45 @@ import SwiftCrossUI
 @HotReloadable
 struct CounterApp: App {
     @State var count = 0
+    @State var theme: ColorScheme = .dark
+
+    @Environment(\.openURL) var openURL
 
     var body: some Scene {
         WindowGroup("CounterExample: \(count)") {
             #hotReloadable {
-                HStack(spacing: 20) {
-                    Button("-") {
-                        count -= 1
-                    }
-                    Text("Count: \(count)")
-                    Button("+") {
-                        count += 1
+                ZStack {
+                    theme == .dark ? Color.black : Color.white
+
+                    VStack {
+                        HStack(spacing: 20) {
+                            Button("-") {
+                                count -= 1
+                            }
+                            Text("Count: \(count)")
+                            Button("+") {
+                                count += 1
+                            }
+                        }
+                        .padding()
+
+                        Menu("Settings") {
+                            Text("Current theme: \(theme)")
+                            Menu("Theme") {
+                                Button("Light") {
+                                    theme = .light
+                                }
+                                Button("Dark") {
+                                    theme = .dark
+                                }
+                            }
+                            Button("Open documentation") {
+                                openURL(URL(string: "https://stackotter.dev")!)
+                            }
+                        }
                     }
                 }
-                .padding()
+                .colorScheme(.dark)
             }
         }
         .defaultSize(width: 400, height: 200)
