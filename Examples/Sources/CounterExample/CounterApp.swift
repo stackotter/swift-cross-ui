8416e83ed33e78fda49442d808194adb1627dc87
stackotter <stackotter@stackotter.dev>
2025-03-30 11:28:22 +1000

    Revert CounterExample, accidentally committed experiment

--- Examples/Sources/CounterExample/CounterApp.swift
+++ Examples/Sources/CounterExample/CounterApp.swift
@@ -1,5 +1,4 @@
 import DefaultBackend
-import Foundation
 import SwiftCrossUI
 
 #if canImport(SwiftBundlerRuntime)
@@ -10,45 +9,22 @@ import SwiftCrossUI
 @HotReloadable
 struct CounterApp: App {
     @State var count = 0
-    @State var theme: ColorScheme = .dark
-
-    @Environment(\.openURL) var openURL
 
     var body: some Scene {
         WindowGroup("CounterExample: \(count)") {
             #hotReloadable {
-                ZStack {
-                    theme == .dark ? Color.black : Color.white
-
-                    VStack {
-                        HStack(spacing: 20) {
-                            Button("-") {
-                                count -= 1
-                            }
-                            Text("Count: \(count)")
-                            Button("+") {
-                                count += 1
-                            }
+                VStack {
+                    HStack(spacing: 20) {
+                        Button("-") {
+                            count -= 1
                         }
-                        .padding()
-
-                        Menu("Settings") {
-                            Text("Current theme: \(theme)")
-                            Menu("Theme") {
-                                Button("Light") {
-                                    theme = .light
-                                }
-                                Button("Dark") {
-                                    theme = .dark
-                                }
-                            }
-                            Button("Open documentation") {
-                                openURL(URL(string: "https://stackotter.dev")!)
-                            }
+                        Text("Count: \(count)")
+                        Button("+") {
+                            count += 1
                         }
                     }
+                    .padding()
                 }
-                .colorScheme(.dark)
             }
         }
         .defaultSize(width: 400, height: 200)
