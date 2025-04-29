3e6530d324edf666bc3d1b6eb293063f33586b79
bbrk24 <bbrk24@gmail.com>
2025-04-28 21:51:11 -0400

    Fix doc comments

--- Sources/SwiftCrossUI/TextContentType.swift
+++ Sources/SwiftCrossUI/TextContentType.swift
@@ -8,30 +8,31 @@ public enum TextContentType {
     /// For numbers that may include decimals or negative numbers, see ``decimal(signed:)``.
     ///
     /// If `ascii` is true, the user should only enter the ASCII digits 0-9. If `ascii` is
-    /// false, they may see a different numeric keypad depending on their locale settings
-    /// (for example, they may see the digits ० १ २ ३ ४ ५ ६ ७ ८ ९ instead if the language
-    /// is set to Hindi).
+    /// false, on mobile devices they may see a different numeric keypad depending on their
+    /// locale settings (for example, they may see the digits ० १ २ ३ ४ ५ ६ ७ ८ ९ instead
+    /// if the language is set to Hindi).
     case digits(ascii: Bool)
     /// A URL.
     ///
-    /// This keyboard type typically has prominent buttons for "/" and ".com", and might not
-    /// include a spacebar.
+    /// On mobile devices, this type shows a keyboard with prominent buttons for "/" and ".com",
+    /// and might not include a spacebar.
     case url
     /// A phone number.
-    ///
-    /// This input type has buttons for the digits 0-9 and the symbols "#" and "*", and may
-    /// include other symbols or even letters.
     case phoneNumber
     /// A person's name.
+    ///
+    /// This typically uses the default keyboard, but informs autocomplete to use contact
+    /// names rather than regular words.
     case name
     /// A number.
     ///
-    /// This keyboard type is guaranteed to have at least digits and a decimal separator. If
-    /// `signed` is true, the input will also have a plus sign and minus sign. Other characters
-    /// may also appear.
+    /// If `signed` is false, on mobile devices it shows a numeric keypad with a decimal point,
+    /// but not necessarily plus and minus signs. If `signed` is true then more punctuation can
+    /// be entered.
     case decimal(signed: Bool)
     /// An email address.
     ///
-    /// This keyboard type typically has prominent buttons for "@", ".", and/or ".com".
+    /// This informs autocomplete that the input is an email address, and on mobile devices,
+    /// displays a keyboard with prominent "@" and "." buttons.
     case emailAddress
 }
