853abd81853733d945de739b0d62431b9b840ea1
stackotter <stackotter@stackotter.dev>
2025-06-24 15:05:09 +1000

    Introduce 'inspect' modifiers for accessing underlying native widgets
    These changes make platform-specific native customizations significantly
    easier to perform. Hopefully this will make SwiftCrossUI significantly
    more viable for actual production apps that often just need to get
    things working even if there's not a nice neat first party API for it
    yet.
    
    Inspiration was taken from swiftui-introspect.

---
+++ Sources/GtkBackend/InspectionModifiers.swift
@@ -0,0 +1,141 @@
+import Gtk
+import SwiftCrossUI
+
+extension View {
+    public func inspect(
+        _ inspectionPoints: InspectionPoints = .onCreate,
+        _ action: @escaping @MainActor @Sendable (Gtk.Widget) -> Void
+    ) -> some View {
+        InspectView(child: self, inspectionPoints: inspectionPoints, action: action)
+    }
+}
+
+extension SwiftCrossUI.Button {
+    public func inspect(
+        _ inspectionPoints: InspectionPoints = .onCreate,
+        _ action: @escaping @MainActor @Sendable (Gtk.Button) -> Void
+    ) -> some View {
+        InspectView(child: self, inspectionPoints: inspectionPoints, action: action)
+    }
+}
+
+extension Text {
+    public func inspect(
+        _ inspectionPoints: InspectionPoints = .onCreate,
+        _ action: @escaping @MainActor @Sendable (Gtk.Label) -> Void
+    ) -> some View {
+        InspectView(child: self, inspectionPoints: inspectionPoints, action: action)
+    }
+}
+
+extension Slider {
+    public func inspect(
+        _ inspectionPoints: InspectionPoints = .onCreate,
+        _ action: @escaping @MainActor @Sendable (Gtk.Scale) -> Void
+    ) -> some View {
+        InspectView(child: self, inspectionPoints: inspectionPoints, action: action)
+    }
+}
+
+extension Picker {
+    public func inspect(
+        _ inspectionPoints: InspectionPoints = .onCreate,
+        _ action: @escaping @MainActor @Sendable (Gtk.DropDown) -> Void
+    ) -> some View {
+        InspectView(child: self, inspectionPoints: inspectionPoints, action: action)
+    }
+}
+
+extension TextField {
+    public func inspect(
+        _ inspectionPoints: InspectionPoints = .onCreate,
+        _ action: @escaping @MainActor @Sendable (Gtk.Entry) -> Void
+    ) -> some View {
+        InspectView(child: self, inspectionPoints: inspectionPoints, action: action)
+    }
+}
+
+extension ScrollView {
+    public func inspect(
+        _ inspectionPoints: InspectionPoints = .onCreate,
+        _ action: @escaping @MainActor @Sendable (Gtk.ScrolledWindow) -> Void
+    ) -> some View {
+        InspectView(child: self, inspectionPoints: inspectionPoints, action: action)
+    }
+}
+
+extension List {
+    public func inspect(
+        _ inspectionPoints: InspectionPoints = .onCreate,
+        _ action: @escaping @MainActor @Sendable (Gtk.ListBox) -> Void
+    ) -> some View {
+        InspectView(child: self, inspectionPoints: inspectionPoints, action: action)
+    }
+}
+
+extension NavigationSplitView {
+    public func inspect(
+        _ inspectionPoints: InspectionPoints = .onCreate,
+        _ action: @escaping @MainActor @Sendable (Gtk.Paned) -> Void
+    ) -> some View {
+        InspectView(child: self, inspectionPoints: inspectionPoints) { (view: Gtk.Fixed) in
+            action(view.children[0] as! Gtk.Paned)
+        }
+    }
+}
+
+extension SwiftCrossUI.Image {
+    public func inspect(
+        _ inspectionPoints: InspectionPoints = .onCreate,
+        _ action: @escaping @MainActor @Sendable (Gtk.Picture) -> Void
+    ) -> some View {
+        InspectView(child: self, inspectionPoints: inspectionPoints) { (_: Gtk.Widget, children: ImageChildren) in
+            action(children.imageWidget.into())
+        }
+    }
+}
+
+extension HStack {
+    public func inspect(
+        _ inspectionPoints: InspectionPoints = .onCreate,
+        _ action: @escaping @MainActor @Sendable (Gtk.Fixed) -> Void
+    ) -> some View {
+        InspectView(child: self, inspectionPoints: inspectionPoints, action: action)
+    }
+}
+
+extension VStack {
+    public func inspect(
+        _ inspectionPoints: InspectionPoints = .onCreate,
+        _ action: @escaping @MainActor @Sendable (Gtk.Fixed) -> Void
+    ) -> some View {
+        InspectView(child: self, inspectionPoints: inspectionPoints, action: action)
+    }
+}
+
+extension ZStack {
+    public func inspect(
+        _ inspectionPoints: InspectionPoints = .onCreate,
+        _ action: @escaping @MainActor @Sendable (Gtk.Fixed) -> Void
+    ) -> some View {
+        InspectView(child: self, inspectionPoints: inspectionPoints, action: action)
+    }
+}
+
+extension Group {
+    public func inspect(
+        _ inspectionPoints: InspectionPoints = .onCreate,
+        _ action: @escaping @MainActor @Sendable (Gtk.Fixed) -> Void
+    ) -> some View {
+        InspectView(child: self, inspectionPoints: inspectionPoints, action: action)
+    }
+}
+
+extension Shape {
+    public func inspect(
+        _ inspectionPoints: InspectionPoints = .onCreate,
+        _ action: @escaping @MainActor @Sendable (Gtk.DrawingArea) -> Void
+    ) -> some View {
+        InspectView(child: self, inspectionPoints: inspectionPoints, action: action)
+    }
+}
