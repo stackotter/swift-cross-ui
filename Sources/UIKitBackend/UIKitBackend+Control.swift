6b4dfcf33ed1ebfe27ea300d0157cdb3b5690da5
Vitor Silveira <ext.vitor.silveira@farfetch.com>
2025-06-23 21:59:11 -0300

    #188: Make the method only executable on iOS

--- Sources/UIKitBackend/UIKitBackend+Control.swift
+++ Sources/UIKitBackend/UIKitBackend+Control.swift
@@ -317,17 +317,17 @@ extension UIKitBackend {
             } else {
                 textEditorWidget.child.inputAccessoryView = nil
             }
-        #endif
 
-        textEditorWidget.child.alwaysBounceVertical = environment.scrollDismissesKeyboardMode != .never
-        textEditorWidget.child.keyboardDismissMode = switch environment.scrollDismissesKeyboardMode {
-        case .immediately:
-            textEditorWidget.child.inputAccessoryView == nil ? .onDrag : .onDragWithAccessory
-        case .interactively:
-            textEditorWidget.child.inputAccessoryView == nil ? .interactive : .interactiveWithAccessory
-        case .never:
-                .none
-        }
+            textEditorWidget.child.alwaysBounceVertical = environment.scrollDismissesKeyboardMode != .never
+            textEditorWidget.child.keyboardDismissMode = switch environment.scrollDismissesKeyboardMode {
+            case .immediately:
+                textEditorWidget.child.inputAccessoryView == nil ? .onDrag : .onDragWithAccessory
+            case .interactively:
+                textEditorWidget.child.inputAccessoryView == nil ? .interactive : .interactiveWithAccessory
+            case .never:
+                    .none
+            }
+        #endif
     }
 
     public func setContent(ofTextEditor textEditor: Widget, to content: String) {
