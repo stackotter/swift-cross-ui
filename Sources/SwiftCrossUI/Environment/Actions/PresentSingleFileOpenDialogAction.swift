734b5dfd2377e7c02faf55c96c2227d693c07ecc
bbrk24 <bbrk24@gmail.com>
2025-11-01 08:32:25 -0400

    Implement file picker in UIKit (#236)
    Huge thanks to @timi2506 on the Discord for pointing out that public.data means "literally any file type"

--- Sources/SwiftCrossUI/Environment/Actions/PresentSingleFileOpenDialogAction.swift
+++ Sources/SwiftCrossUI/Environment/Actions/PresentSingleFileOpenDialogAction.swift
@@ -3,6 +3,7 @@ import Foundation
 /// Presents an 'Open file' dialog fit for selecting a single file. Some
 /// backends only allow selecting either files or directories but not both
 /// in a single dialog. Returns `nil` if the user cancels the operation.
+@available(tvOS, unavailable, message: "tvOS does not provide file system access")
 public struct PresentSingleFileOpenDialogAction: Sendable {
     let backend: any AppBackend
     let window: MainActorBox<Any?>
