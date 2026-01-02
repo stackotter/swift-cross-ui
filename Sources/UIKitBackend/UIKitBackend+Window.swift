b8d64791d487b1fdbbccbecf793d0e10cf3fd566
Kaleb A. Ascevich <lintel-stylish.0r@icloud.com>
2026-01-02 10:45:37 -0500

    stub `close(window:)` and `setCloseHandler(ofWindow:to:)` in UIKitBackend

--- Sources/UIKitBackend/UIKitBackend+Window.swift
+++ Sources/UIKitBackend/UIKitBackend+Window.swift
@@ -121,6 +121,17 @@ extension UIKitBackend {
         window.makeKeyAndVisible()
     }
 
+    public func close(window: Window) {
+        print("UIKitBackend: ignoring \(#function) call")
+    }
+
+    public func setCloseHandler(
+        ofWindow window: Window,
+        to action: @escaping () -> Void
+    ) {
+        print("UIKitBackend: ignoring \(#function) call")
+    }
+
     public func isWindowProgrammaticallyResizable(_ window: Window) -> Bool {
         #if os(visionOS)
             true
