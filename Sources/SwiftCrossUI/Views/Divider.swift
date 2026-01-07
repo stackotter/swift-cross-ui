923f319cf5bb618c7cf100e925de0fe5230c4da4
Kaleb A. Ascevich <lintel-stylish.0r@icloud.com>
2026-01-07 11:34:47 -0500

    make `Divider` sendable

--- Sources/SwiftCrossUI/Views/Divider.swift
+++ Sources/SwiftCrossUI/Views/Divider.swift
@@ -1,7 +1,7 @@
 /// A divider that expands along the minor axis of the containing stack layout
 /// (or horizontally otherwise). In dark mode it's white with 10% opacity, and
 /// in light mode it's black with 10% opacity.
-public struct Divider: View {
+public struct Divider: View, Sendable {
     @Environment(\.colorScheme) var colorScheme
     @Environment(\.layoutOrientation) var layoutOrientation
 
