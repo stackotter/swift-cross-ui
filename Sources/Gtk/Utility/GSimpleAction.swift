4caa40e6e324ed52237f27a962c6f288617b37bf
Kaleb A. Ascevich <lintel-stylish.0r@icloud.com>
2026-01-08 10:16:35 -0500

    simplify GSimpleAction

--- Sources/Gtk/Utility/GSimpleAction.swift
+++ Sources/Gtk/Utility/GSimpleAction.swift
@@ -9,37 +9,18 @@ public class GSimpleAction: GAction, GObjectRepresentable {
 
     @GObjectProperty(named: "enabled") var enabled: Bool
 
-    private typealias ActionHandler =
-    @convention(c) (
-        UnsafeMutableRawPointer,
-        OpaquePointer,
-        UnsafeMutableRawPointer
-    ) -> Void
+    private class Box<T> {
+        var value: T
+        init(_ value: T) { self.value = value }
+    }
 
     public init(name: String, action: @escaping () -> Void) {
         actionPointer = g_simple_action_new(name, nil)
 
-        class Action {
-            var action: () -> Void
-            init(_ action: @escaping () -> Void) { self.action = action }
+        connectActionSignal(Box(action)) { _, _, data in
+            let action = Unmanaged<Box<() -> Void>>.fromOpaque(data).takeUnretainedValue()
+            action.value()
         }
-        let wrappedAction = Action(action)
-
-        let handler: ActionHandler = { _, _, data in
-            let action = Unmanaged<Action>.fromOpaque(data).takeUnretainedValue()
-            action.action()
-        }
-
-        g_signal_connect_data(
-            UnsafeMutableRawPointer(actionPointer),
-            "activate",
-            gCallback(handler),
-            Unmanaged<Action>.passRetained(wrappedAction).toOpaque(),
-            { data, _ in
-                Unmanaged<Action>.fromOpaque(data!).release()
-            },
-            G_CONNECT_AFTER
-        )
     }
 
     public init(name: String, state: Bool, action: @escaping (Bool) -> Void) {
@@ -49,13 +30,7 @@ public class GSimpleAction: GAction, GObjectRepresentable {
             g_variant_new_boolean(state.toGBoolean())
         )
 
-        class Action {
-            var action: (Bool) -> Void
-            init(_ action: @escaping (Bool) -> Void) { self.action = action }
-        }
-        let wrappedAction = Action(action)
-
-        let handler: ActionHandler = { actionPointer, _, data in
+        connectActionSignal(Box(action)) { actionPointer, _, data in
             // Get the current state...
             let stateVariant = g_action_get_state(OpaquePointer(actionPointer))
             let state = g_variant_get_boolean(stateVariant).toBool()
@@ -68,17 +43,27 @@ public class GSimpleAction: GAction, GObjectRepresentable {
             )
 
             // ...then get the action and call it with the new state.
-            let action = Unmanaged<Action>.fromOpaque(data).takeUnretainedValue()
-            action.action(newState)
+            let action = Unmanaged<Box<(Bool) -> Void>>.fromOpaque(data).takeUnretainedValue()
+            action.value(newState)
         }
+    }
 
+    private func connectActionSignal(
+        _ value: some AnyObject,
+        handler:
+            @convention(c) (
+                UnsafeMutableRawPointer,
+                OpaquePointer,
+                UnsafeMutableRawPointer
+            ) -> Void
+    ) {
         g_signal_connect_data(
             UnsafeMutableRawPointer(actionPointer),
             "activate",
             gCallback(handler),
-            Unmanaged<Action>.passRetained(wrappedAction).toOpaque(),
+            Unmanaged.passRetained(value).toOpaque(),
             { data, _ in
-                Unmanaged<Action>.fromOpaque(data!).release()
+                Unmanaged<AnyObject>.fromOpaque(data!).release()
             },
             G_CONNECT_AFTER
         )
