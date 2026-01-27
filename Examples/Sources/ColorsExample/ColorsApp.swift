2411bbeca58673e3606a014f3baa3d569d685da4
Kaleb A. Ascevich <lintel-stylish.0r@icloud.com>
2026-01-26 22:55:06 -0500

    enable showing SCUI colors as well as system colors in ColorsExample

--- Examples/Sources/ColorsExample/ColorsApp.swift
+++ Examples/Sources/ColorsExample/ColorsApp.swift
@@ -8,34 +8,63 @@ import SwiftCrossUI
 @main
 @HotReloadable
 struct ColorsApp: App {
-    @State var count = 0
+    @State private var isShowingSystemColors = false
 
     var body: some Scene {
         WindowGroup("ColorsExample") {
             #hotReloadable {
                 VStack {
+                    Toggle("Show system colors", isOn: $isShowingSystemColors)
                     Text("First row is .dark, second row is .light, third row is the system scheme")
+                    Text(
+                        """
+                        If "Show system colors" is on, platform-specific colors will be used; \
+                        otherwise SCUI's built-in colors will be shown
+                        """
+                    )
 
                     ScrollView(.horizontal) {
                         VStack(spacing: 5) {
-                            let colors: [Color.SystemAdaptive] = [
-                                .blue,
-                                .brown,
-                                .gray,
-                                .green,
-                                .orange,
-                                .purple,
-                                .red,
-                                .yellow,
-                            ]
+                            let colors: [Color] =
+                                if isShowingSystemColors {
+                                    [
+                                        .system(.blue),
+                                        .system(.brown),
+                                        .system(.gray),
+                                        .system(.green),
+                                        .system(.orange),
+                                        .system(.purple),
+                                        .system(.red),
+                                        .system(.yellow),
+                                    ]
+                                } else {
+                                    [
+                                        .blue,
+                                        .brown,
+                                        .gray,
+                                        .green,
+                                        .orange,
+                                        .purple,
+                                        .red,
+                                        .yellow,
+
+                                        // Add the SCUI-exclusive colors to the end so as
+                                        // to make comparing the non-exclusive colors easier.
+                                        .cyan,
+                                        .indigo,
+                                        .mint,
+                                        .pink,
+                                        .teal,
+                                    ]
+                                }
 
                             let colorStack = HStack(spacing: 5) {
                                 ForEach(colors, id: \.self) { color in
                                     VStack {
-                                        Color.system(color)
-                                            .aspectRatio(1, contentMode: .fill)
+                                        color.aspectRatio(1, contentMode: .fill)
 
                                         #if os(tvOS)
+                                            // Add something focusable so we can scroll on tvOS.
                                             Button("focus this") {}
                                         #endif
                                     }
