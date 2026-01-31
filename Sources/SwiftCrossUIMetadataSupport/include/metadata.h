388b22e0f8861a2138e475fb0390028f5c26471a
Kaleb A. Ascevich <lintel-stylish.0r@icloud.com>
2026-01-31 13:01:20 -0500

    update `_getSwiftBundlerMetadata()` documentation

--- Sources/SwiftCrossUIMetadataSupport/include/metadata.h
+++ Sources/SwiftCrossUIMetadataSupport/include/metadata.h
@@ -1,8 +1,26 @@
-/// Returns an untyped pointer to UTF8-encoded JSON data.
+/// Returns an untyped pointer to a Swift `[[UInt8]]` containing
+/// UTF8-encoded JSON data in its first element.
 ///
-/// Bridges to Swift as:
+/// This function bridges to Swift as:
 ///
 /// ```swift
 /// func _getSwiftBundlerMetadata() -> UnsafeRawPointer?
 /// ```
+///
+/// The metadata can be accessed by binding the returned pointer to `[[UInt8]]`:
+///
+/// ```swift
+/// guard let pointer = _getSwiftBundlerMetadata() else { return }
+/// let datas = pointer.assumingMemoryBound(to: [[UInt8]].self).pointee
+/// ```
+///
+/// > Important: Attempting to call this function when it has not been linked in
+/// > by swift-bundler will cause linking to fail. To prevent this, wrap all code
+/// > paths relying on this function in:
+/// >
+/// > ```swift
+/// > #if SWIFT_BUNDLER_METADATA
+/// >   // ...
+/// > #endif
+/// > ```
 const void * _Nullable _getSwiftBundlerMetadata();
